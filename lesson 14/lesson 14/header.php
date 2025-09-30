9f2059b415657707ce861cb1dc49d21cf79a0233
arlindjashari1 <grajzi.consulting@gmail.com>
2025-09-30 17:39:27 +0200

    d

---
+++ lesson 14/lesson 14/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Digital School</title>
+    <link rel="stylesheet" href0="css/bootstrap.css">
+    <link rel= "stylesheet" href="css/main.css">
+</head>
+<body>
+    
